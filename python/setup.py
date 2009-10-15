f1a5df0f85170522e74812623670fcf35651d159
Tony Garnock-Jones <tonyg@kcbbs.gen.nz>
2009-10-15 18:55:38 +0100

    Python infrastructure

---
+++ python/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import os
+
+setup(name='json-shapes',
+      version='0.1',
+      description='JSON-Shapes Library',
+      author='Tony Garnock-Jones',
+      author_email='tonyg@kcbbs.gen.nz',
+      url='http://github.com/tonyg/json-shapes/',
+      packages=['jsonshapes'],
+      )
